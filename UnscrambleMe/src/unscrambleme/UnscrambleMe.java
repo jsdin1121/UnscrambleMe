a14569c6f6facba6b2439a758b843c349c36e6aa
Janice <jsdin1121@gmail.com>
2021-12-12 22:57:09 -0500

    my commit

--- UnscrambleMe/src/unscrambleme/UnscrambleMe.java
+++ UnscrambleMe/src/unscrambleme/UnscrambleMe.java
@@ -57,7 +57,7 @@ public class UnscrambleMe {
                         System.out.printf("%s%s\n\n", "Sorry, you're out of tries. The answer was: ", line);
                     }
                     else if(count==3){
-                        System.out.println("Hint: the word starts with the letter: " + line.charAt(0)); //char index 0
+                        System.out.println("Hint: the word starts with the letter: " + line.charAt(0));
                     }
 
             }
